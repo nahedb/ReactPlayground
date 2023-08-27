6ff66d4c1002296dfdf45298f8e767a428992e86
nahedb <brendan.n.nahed@gmail.com>
2023-08-26 23:56:50 -0400

    Mapping lists to a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import * as React from 'react'
+//import * as React from 'react'
 
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-}
-
-function getTitle(title){
-  return title;
-}
+const contract = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://reduc.js.org/',
+    author: 'Dan Abramvo, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
       <div>
-        <hi>{getTitle(welcome.title)}</hi>
+        <hi>My Hacker Stories</hi>
         <label htmlFor="search"> Search: </label>
         <input id="search" type="text" />
+
+        <hr />
+        <ul>
+          {contract.map(function(item){
+            return <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              </li>;
+          })}
+        </ul>
     </div>
   );
 }
